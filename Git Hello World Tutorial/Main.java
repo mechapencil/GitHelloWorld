9c019caed41e4ecd36bd15176eb12cecb25edb9a
mechapencil <46091431+mechapencil@users.noreply.github.com>
2019-03-27 15:28:59 -0400

    Update Main.java

--- Git Hello World Tutorial/Main.java
+++ Git Hello World Tutorial/Main.java
@@ -3,12 +3,20 @@
 
     To run this program easily, try a browser-based IDE like
     https://repl.it/languages/java
+    
+    This file is the 'main,' the primary file that runs the whole program.
 */
 class Main {
   public static void main(String[] args) {
 
-    Speaker speaker = new Speaker();
+    //Initialize a Speaker object, named publicSpeaker, and set it equal to
+    //the value of a new default Speaker.
+    Speaker publicSpeaker = new Speaker();
 
-    System.out.println(Speaker.text);
+    //Print out text to the console. The text in the parentheses are called
+    //arguments. This function accepts string values, and variables that hold
+    //string values. The argument we provide here is the 'text' string variable
+    //belonging to our publicSpeaker object.
+    System.out.println(publicSpeaker.text);
   }
 }
